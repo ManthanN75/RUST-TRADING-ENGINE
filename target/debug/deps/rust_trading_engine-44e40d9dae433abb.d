d1ffaace2244f1ab9669d27fc44d3bb424b70fe8
ManthanN75 <achauhan25263@gmail.com>
2026-03-11 03:34:37 +0000

    added tests for market order smaller than limit order and other for interconnectedness

---
+++ target/debug/deps/rust_trading_engine-44e40d9dae433abb.d
@@ -0,0 +1,8 @@
+D:\rust\rust-trading-engine\target\debug\deps\rust_trading_engine-44e40d9dae433abb.d: src\main.rs src\matching_engine\mod.rs src\matching_engine\engine.rs src\matching_engine\orderbook.rs
+
+D:\rust\rust-trading-engine\target\debug\deps\rust_trading_engine-44e40d9dae433abb.exe: src\main.rs src\matching_engine\mod.rs src\matching_engine\engine.rs src\matching_engine\orderbook.rs
+
+src\main.rs:
+src\matching_engine\mod.rs:
+src\matching_engine\engine.rs:
+src\matching_engine\orderbook.rs:
